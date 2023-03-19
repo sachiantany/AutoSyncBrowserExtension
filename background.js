85cda00a94647e4ecbbcf4ea11d42f5f059c4203
sachiantany <sachi.antany@gmail.com>
2023-03-19 13:11:16 +0530

    commented 1

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
+//Handle the other URL permission error
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.error && message.error.includes("Cannot access a chrome:// URL")) {
     console.error("Error: Cannot access a chrome:// URL");
   }
 });
 
+//Work on installing the Ext
 chrome.runtime.onInstalled.addListener(function() {
   // Set default values in storage
   chrome.storage.sync.set({ "frequency": '', "timer": '' }, function() {
@@ -12,7 +14,7 @@ chrome.runtime.onInstalled.addListener(function() {
 
 });
 
-
+//Work on creating a new tab
 chrome.tabs.onCreated.addListener(function(tabs) {
   chrome.storage.sync.get(['frequency','timer'], function(result) {
     const frequency = result.frequency;
@@ -27,6 +29,7 @@ chrome.tabs.onCreated.addListener(function(tabs) {
   });
 });
 
+//Work on updating the tab
 chrome.tabs.onUpdated.addListener(function(tabs) {
   console.log("On update!");
   chrome.storage.sync.get(['frequency','timer'], function(data) {
